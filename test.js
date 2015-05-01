13f334ecee1ee1e7a2c69398ddcdce89c401aa08
Richard Fox <richard.fox@ahref.co.uk>
2015-05-01 18:58:24 +0100

    blah?

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+require('es6-shim');
+
 var State = require('./State');
 var Director = require('./Director');
 
@@ -12,25 +14,28 @@ var finder = new Finder();
 test.updateState(testState);
 finder.update(test);
 
-console.log(test.getSplitBoard());
+// console.log(test.getSplitBoard());
+// console.log(finder.grid.formatGrid());
 
 //console.log(test.findImpassable());
 
 // console.log(test.getHero().id);
 
-// console.log(test.findClosest(test.getHero().pos,test.findEnemyMines()));
+console.log(test.getHeroByPos(test.getHero().pos));
+
+//console.log(test.findClosest(test.getHero().pos,test.findEnemyMines()));
 
 // console.log(test.getPair(test.posToIndex({x:5,y:6})));
 // // 
-//console.log('Im thirsty lets find a path to a tavern');
-var tavern = test.findClosest(test.getHero().pos,test.findTaverns());
-//console.log('I am at: ');
-console.log(test.getHero().pos);
-//console.log('The tavern is at: ');
-console.log(tavern);
-//console.log('I think out next step is ');
+// //console.log('Im thirsty lets find a path to a tavern');
+// var tavern = test.findClosest(test.getHero().pos,test.findTaverns());
+// //console.log('I am at: ');
+// console.log(test.getHero().pos);
+// //console.log('The tavern is at: ');
+// console.log(tavern);
+// //console.log('I think out next step is ');
 
-console.log(finder.directionTo(test.getHero().pos,{x:6,y:8}));
+// console.log(finder.directionTo(test.getHero().pos,tavern));
 
 
 
